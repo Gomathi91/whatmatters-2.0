ea53e7fe2f81b02a45a3f12fda3b7d12a6dc5554
Gomathi <gomathi.p@spagylo.com>
2025-10-22 12:53:14 +0530

    preview code updated

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,57 +1,26 @@
-import { deliveryclient, previewclient } from "@/utils/contentful";
-import ImageBlock from "@/components/Blocks/ImageBlock";
-import TextImageBlock from "@/components/Blocks/ImageTextBlock";
-import FullWidthTextBlock from "@/components/Blocks/TextBlock";
-
-function BlockRenderer({ block }) {
-  const contentType = block.sys.contentType?.sys?.id;
-  if (!contentType) return null;
-
-  switch (contentType) {
-    case "fullWidthImageBlock":
-      return <ImageBlock data={block.fields} />;
-    case "textImageBlock":
-      return <TextImageBlock data={block.fields} />;
-    case "fullWidthTextBlock":
-      return <FullWidthTextBlock data={block.fields} />;
-    default:
-      return null;
-  }
-}
+import { deliveryclient, previewclient } from '@/utils/contentful';
+import PageContent from '@/components/PageContent';
 
 export default async function Home({ searchParams }) {
+ const params = await searchParams; 
 
-  const params = await searchParams;
-  const isPreview = params?.preview === "true";
+  const isPreview = params?.preview === 'true';
   const slug = params?.slug;
 
   const client = isPreview ? previewclient : deliveryclient;
 
   const res = await client.getEntries({
-    content_type: "deluxePage",
-    "fields.slug": slug,
+    content_type: 'deluxePage',
+    'fields.slug': slug,
     include: 3,
   });
+
   const posts = res.items;
 
   if (!posts || posts.length === 0) {
     return <p>No content found.</p>;
   }
 
-  return (
-    <div>
-      <div>
-        <a className="heading">What Matters</a>
-      </div>
-      {/* <h1>{isPreview ? "Preview Mode" : "Published Mode"}</h1> */}
-      {posts.map((post) => (
-        <div key={post.sys.id}>
-          {/* <h2>{post.fields.title}</h2> */}
-          {post.fields.pageBlocks?.map((block, index) => (
-            <BlockRenderer key={block.sys.id || index} block={block} />
-          ))}
-        </div>
-      ))}
-    </div>
-  );
+  // Pass data & theme down to client component
+  return <PageContent post={posts[0]} isPreview={isPreview} />;
 }
---
+++ src/components/PageContent.js
@@ -0,0 +1,49 @@
+'use client';
+
+import {
+  ContentfulLivePreviewProvider,
+  useContentfulLiveUpdates,
+  useContentfulInspectorMode,
+} from '@contentful/live-preview/react';
+
+import ImageBlock from '@/components/Blocks/ImageBlock';
+import TextImageBlock from '@/components/Blocks/ImageTextBlock';
+import FullWidthTextBlock from '@/components/Blocks/TextBlock';
+
+function BlockRenderer({ block }) {
+  const contentType = block.sys.contentType?.sys?.id;
+  if (!contentType) return null;
+
+  switch (contentType) {
+    case 'fullWidthImageBlock':
+      return <ImageBlock data={block.fields} />;
+    case 'textImageBlock':
+      return <TextImageBlock data={block.fields} />;
+    case 'fullWidthTextBlock':
+      return <FullWidthTextBlock data={block.fields} />;
+    default:
+      return null;
+  }
+}
+
+export default function PageContent({ post, isPreview }) {
+  const updatedPost = useContentfulLiveUpdates(post);
+  const inspectorProps = useContentfulInspectorMode({ entryId: post.sys.id });
+
+  return (
+    <ContentfulLivePreviewProvider
+      locale="en-US"
+      enableLiveUpdates={isPreview}
+      enableInspectorMode={isPreview}
+    >
+      <div {...inspectorProps({ fieldId: 'title' })}>
+        <div>
+          <a className="heading">What Matters</a>
+        </div>
+        {updatedPost.fields.pageBlocks?.map((block, index) => (
+          <BlockRenderer key={block.sys.id || index} block={block} />
+        ))}
+      </div>
+    </ContentfulLivePreviewProvider>
+  );
+}
