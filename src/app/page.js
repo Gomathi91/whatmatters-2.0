7a72e0b5b9f4ab5cc901e0665f82105fd95f92db
Gomathi <gomathi.p@spagylo.com>
2025-10-22 18:49:51 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,27 @@
 import React from 'react'
-import Home from './home/page'
+import { getContentfulClient} from '@/utils/contentful';
+import PageContent from '@/components/PageContent';
 
-function page() {
-  return (
-    <div>
-      <Home/>
-    </div>
-  )
+export default async function page({ searchParams }) {
+  const params = await searchParams; 
+  
+    const isPreview = params?.preview === 'true';
+    const slug = params?.slug;
+  
+    const client = getContentfulClient(isPreview);
+  
+    const res = await client.getEntries({
+      content_type: 'deluxePage',
+      'fields.slug': slug,
+      include: 3,
+    });
+  
+    const posts = res.items;
+  
+    if (!posts || posts.length === 0) {
+      return <p>No content found.</p>;
+    }
+  
+    // Pass data & theme down to client component
+    return <PageContent post={posts[0]} isPreview={isPreview} />;
 }
-
-export default page
